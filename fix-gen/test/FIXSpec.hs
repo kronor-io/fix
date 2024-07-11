0d8a462ebb5f1f53c5393c98c2dc204e707ff199
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-07-11 16:09:35 +0200

    field type

--- fix-gen/test/FIXSpec.hs
+++ fix-gen/test/FIXSpec.hs
@@ -7,6 +7,7 @@ module FIXSpec (spec) where
 import Control.Monad
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as SB
+import Data.Typeable
 import FIX
 import FIX.Gen ()
 import Path
@@ -31,6 +32,9 @@ spec = do
           let rendered = renderMessage message
           rendered `shouldBe` contents
 
+  fieldTypeSpec @ByteString
+  fieldTypeSpec @Int
+
   describe "TestRequestId" $ do
     fieldSpec @TestRequestId
   describe "EncryptionMethod" $ do
@@ -43,22 +47,47 @@ spec = do
   describe "LogonMessage" $ do
     messageSpec @LogonMessage "logon"
 
+fieldTypeSpec ::
+  forall a.
+  ( Show a,
+    Eq a,
+    GenValid a,
+    Typeable a,
+    IsFieldType a
+  ) =>
+  Spec
+fieldTypeSpec = do
+  genValidSpec @a
+  describe "fromValue" $ do
+    it "roundtrips with toValue" $
+      forAllValid $ \a -> do
+        let rendered = toValue (a :: a)
+        context (ppShow rendered) $ case fromValue rendered of
+          Nothing -> expectationFailure "Failed to parse field type value."
+          Just a' -> a' `shouldBe` a
+  describe "toValue" $ do
+    it "renders to valid messages" $
+      producesValid (toValue :: a -> ByteString)
+
 fieldSpec ::
   forall a.
   ( Show a,
     Eq a,
     GenValid a,
+    Typeable a,
     IsField a
   ) =>
   Spec
-fieldSpec =
+fieldSpec = do
   describe "fieldFromValue" $ do
+    genValidSpec @a
     it "roundtrips with fieldToValue" $
       forAllValid $ \a -> do
         let rendered = fieldToValue (a :: a)
         context (ppShow rendered) $ case fieldFromValue rendered of
-          Nothing -> expectationFailure "Failed to parse message."
+          Nothing -> expectationFailure "Failed to parse field."
           Just a' -> a' `shouldBe` a
+  describe "fieldToValue" $ do
     it "renders to valid messages" $
       producesValid (fieldToValue :: a -> ByteString)
 
@@ -67,11 +96,13 @@ messageSpec ::
   ( Show a,
     Eq a,
     GenValid a,
+    Typeable a,
     IsMessage a
   ) =>
   FilePath ->
   Spec
-messageSpec dir =
+messageSpec dir = do
+  genValidSpec @a
   describe "fromMessage" $ do
     it "roundtrips with toMessage" $
       forAllValid $ \a -> do
@@ -79,23 +110,24 @@ messageSpec dir =
         context (ppShow rendered) $ case fromMessage rendered of
           Nothing -> expectationFailure "Failed to parse message."
           Just a' -> a' `shouldBe` a
+  describe "toMessage" $ do
     it "renders to valid messages" $
       producesValid (toMessage :: a -> Message)
 
-    scenarioDir ("test_resources/messages/" ++ dir) $ \fp -> do
-      af <- resolveFile' fp
-      when (fileExtension af == Just ".tagvalue") $
-        it "can parse this message and roundtrip it" $ do
-          contents <- SB.readFile (fromAbsFile af)
-          case parseMessage contents of
-            Left err -> expectationFailure err
-            Right message -> case fromMessage message of
-              Nothing -> expectationFailure "Could not parse typed message from untyped message"
-              Just a -> do
-                shouldBeValid (a :: a)
-                -- TODO include these tests too:
-                -- let renderedMessage = toMessage a
-                -- renderedMessage `shouldBe` message
-                -- let renderedBytes = renderMessage renderedMessage
-                -- renderedBytes `shouldBe` content
-                pure ()
+  scenarioDir ("test_resources/messages/" ++ dir) $ \fp -> do
+    af <- resolveFile' fp
+    when (fileExtension af == Just ".tagvalue") $
+      it "can parse this message and roundtrip it" $ do
+        contents <- SB.readFile (fromAbsFile af)
+        case parseMessage contents of
+          Left err -> expectationFailure err
+          Right message -> case fromMessage message of
+            Nothing -> expectationFailure "Could not parse typed message from untyped message"
+            Just a -> do
+              shouldBeValid (a :: a)
+              -- TODO include these tests too:
+              -- let renderedMessage = toMessage a
+              -- renderedMessage `shouldBe` message
+              -- let renderedBytes = renderMessage renderedMessage
+              -- renderedBytes `shouldBe` content
+              pure ()
